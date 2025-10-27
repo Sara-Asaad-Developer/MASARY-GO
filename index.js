07366dce5dc957b3089a54afd0fcbe749612adc0
Sara-Asaad-Developer <sarahasaad57@gmail.com>
2025-10-28 00:20:46 +0300

    v1

---
+++ index.js
@@ -0,0 +1,59 @@
+let balSpan = document.querySelector('#balSpan');
+let input = document.querySelector('input');
+let balance = 1000;
+
+let table = document.querySelector('table tbody');
+let statement = [
+
+];
+let showStatement = () => {
+    table.innerHTML = '';
+    statement.forEach((le, index) => {
+        let row = `
+<tr>
+<td>${index + 1}</td>
+<td>${le.beforeBalance}</td>
+<td>${le.amount}</td>
+<td>${le.type}</td>
+<td>${le.afterBalance}</td>
+</tr>
+`;
+        table.innerHTML += row;
+    });
+};
+
+let showBalance = () => {
+    balSpan.innerText = balance + ' ' + 'EGP';
+};
+let deposit = () => {
+    let amount = +input.value;
+    let newStatement = {
+        beforeBalance: balance,
+        amount: amount,
+        type: 'deposit',
+        afterBalance: balance + amount
+    };
+    statement.push(newStatement);
+    balance = balance + amount;
+    showBalance();
+    input.value = '';
+};
+let withdraw = () => {
+    let amount = +input.value;
+    let newStatement = {
+        beforeBalance: balance,
+        amount: amount,
+        type: 'withdraw',
+        afterBalance: balance - amount
+    };
+    statement.push(newStatement);
+    balance = balance - amount;
+    if (amount <= balance) {
+        balance = balance - amount;
+        showBalance();
+    } else {
+        alert('The Operation Is Not Acceptable');
+    }
+    input.value = '';
+};
+
